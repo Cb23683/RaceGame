d21b3211e4903923411f93d3d8ef23634ea930cb
Clayton <cb23683@ms.wasatch.edu>
2021-04-07 13:03:33 -0600

    Input System & TextMesh pt1
    NOT FINISHED! Just doing this so it saves.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private InputHandler inputHandler;
+    private Rigidbody rb;
+    public TMP_Text scoreTxt;
+
+
+    public float speed;
+    public int score = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        inputHandler = InputHandler.instance;
     }
 
+
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(inputHandler.move.x, 0, inputHandler.move.y);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("pickup"))
+        {
+            Destroy(other.gameObject);
+            score++;
+            scoreTxt.SetText("Score: " + score);
+            if (score >= 11)
+            {
+                scoreTxt.SetText("You Win!");
+            }
+        }
     }
 }
