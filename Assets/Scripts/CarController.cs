ad9bb58bb332cc4cef666f93f8fa0b6c45573315
Clayton <cb23683@ms.wasatch.edu>
2021-04-15 12:49:11 -0600

    Car Controller System
    Pt 1 not done yet

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    //get input
+    //use input to move sphere
+   
+
+    public Rigidbody sphereRB;
+    private InputManager input;
+    public float moveInput;
+
+    public float fwdSpeed;
+    public float revSpeed;
+
+
+
+
+    void Start()
+    {
+        input = InputManager.instance;
+        sphereRB.transform.parent = null;
+
+    }
+
+
+    void Update()
+    {
+        moveInput = input.move.y;
+        moveInput *= fwdSpeed > 0 ? fwdSpeed : revSpeed;
+
+        
+        // set cars position to sphere
+        transform.position = sphereRB.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+    }
+}
