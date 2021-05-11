bc1c3fd51665317decfc970aa633358e7fd63584
zachery <zw12923@ms.wasatch.edu>
2021-05-11 12:55:06 -0600

    stuff

---
+++ Assets/_Scenes/Crips/Pickups.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
+    }
+}
